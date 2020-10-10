d5d019ed301eefe22bdcc06c8a36b0504e96bec8
Wilco Hendriksen <wilcohendriksen@hillstar.nl>
2020-10-10 12:52:59 +0200

    init

---
+++ ReadMe.md
@@ -0,0 +1,36 @@
+# Timr
+
+## Goal
+Just a small app to display a timer in OBS studio during football matches. It won't keep score, just a timer.
+
+## Usage
+[S]  
+Starts the timer
+
+[R]  
+Reset the timer to 00:00
+
+[T]  
+Start at 45:00
+
+[X]  
+Stop the time
+
+[Right]  
+Add 1 minute
+
+[Left]  
+Subtract 1 minute
+
+[Up]  
+Up fontsize
+
+[Down]  
+Lower fontsize
+
+On the bottom right there is sizer. You can size the window, the text will always be centralized.
+
+## needed runtime
+Desktop Runtime 3.1.4 versie x64
+
+https://dotnet.microsoft.com/download/dotnet-core/3.1
---
+++ timr/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace timr
+{
+  /// <summary>
+  ///   Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private readonly DispatcherTimer dt;
+    private TimeSpan time;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      dt = new DispatcherTimer();
+      time = TimeSpan.Zero;
+      TimerLabel.Content = time.ToString(@"mm\:ss");
+    }
+
+    private void DragMove(object sender, MouseButtonEventArgs e)
+    {
+      try
+      {
+        DragMove();
+      }
+      catch (Exception ex)
+      {
+        // don't do anything...
+      }
+    }
+
+    private void timr_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.S)
+      {
+        dt.Start();
+      }
+
+      if (e.Key == Key.R)
+      {
+        time = TimeSpan.Zero;
+      }
+
+      if (e.Key == Key.T)
+      {
+        time = TimeSpan.FromMinutes(45);
+      }
+
+      if (e.Key == Key.X)
+      {
+        dt.Stop();
+      }
+
+      if (e.Key == Key.Right)
+      {
+        time += TimeSpan.FromMinutes(1);
+      }
+
+      if (e.Key == Key.Left)
+      {
+        time -= TimeSpan.FromMinutes(1);
+      }
+
+      if (e.Key == Key.Up)
+      {
+        TimerLabel.FontSize++;
+      }
+
+      if (e.Key == Key.Down)
+      {
+        TimerLabel.FontSize--;
+      }
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      dt.Interval = TimeSpan.FromSeconds(1);
+      dt.Tick += (o, args) =>
+      {
+        time += TimeSpan.FromSeconds(1);
+        TimerLabel.Content = time.ToString(@"mm\:ss");
+      };
+    }
+  }
+}
